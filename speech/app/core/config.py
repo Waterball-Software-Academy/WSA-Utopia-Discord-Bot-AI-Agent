0507bce69ca993fd2d35b73c029995351743ee28
Wei Ting Wu <weiting@projectmars.info>
2024-08-19 16:11:39 +0800

    init: setup api project structure

---
+++ speech/app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Speech"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
