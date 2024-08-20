801a05ed845268f71bd07e9f13c2719987e4607f
johnny850807 <johnny850807@gmail.com>
2024-08-20 15:23:35 +0800

    :art: refactor extract duplicate code in WsaModDiscordSpeechHandler.py

--- speech/app/services/WsaModDiscordSpeechHandler.py
+++ speech/app/services/WsaModDiscordSpeechHandler.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Callable, Awaitable
+from typing import Callable, Awaitable, Optional
 
 import discord
 from fastapi import Depends
@@ -11,8 +11,27 @@ from speech.app.data.speech_repo import Dependency as SpeechRepoDependency
 from speech.app.entities.speech_application import SpeechApplication, ApplicationReviewStatus
 
 
+async def handle_application_review_result_interaction(embed: discord.Embed,
+                                                       interaction: discord.Interaction,
+                                                       updated_title: str, response_message: str,
+                                                       deny_reason: Optional[str] = None):
+    embed.title = updated_title
+    embed.description = (f"""{embed.description}---
+審查者：<@{interaction.user.id}>
+審查時間：{convert_to_minguo_format(datetime.now())}
+""")
+    if deny_reason:
+        embed.description = f"{embed.description}拒絕原因：{deny_reason}"
+        embed.colour = discord.Color.red()
+    else:
+        embed.colour = discord.Color.green()
+    await interaction.message.edit(embed=embed, view=None)
+    await interaction.respond(response_message, ephemeral=True)
+
+
 class DenyReasonModal(discord.ui.Modal):
-    def __init__(self, speech_id: str, speech_application_repository: SpeechApplicationRepository, embed):
+    def __init__(self, speech_id: str, speech_application_repository: SpeechApplicationRepository,
+                 embed: discord.Embed):
         super().__init__(title="拒絕申請")
         self.__speech_id = speech_id
         self.__speech_application_repository = speech_application_repository
@@ -21,21 +40,15 @@ class DenyReasonModal(discord.ui.Modal):
         self.add_item(self.input_field)
 
     async def callback(self, interaction: discord.Interaction):
-        reason = self.input_field.value
+        deny_reason = self.input_field.value
         try:
             self.__speech_application_repository.update_speech_application_review_status(self.__speech_id,
                                                                                          ApplicationReviewStatus.DENIED,
-                                                                                         reason)
-            self.__embed.title = "🙅 短講申請審查（已拒絕審查）"
-            self.__embed.description = (f"""{self.__embed.description}---
-審查者：<@{interaction.user.id}>
-審查時間：{convert_to_minguo_format(datetime.now())}
-拒絕原因：{reason}
-""")
-            self.__embed.color = discord.Color.red()
-
-            await interaction.message.edit(embed=self.__embed, view=None)
-            await interaction.respond(f"✅ 短講申請 ({self.__speech_id}) 已被拒絕。", ephemeral=True)
+                                                                                         deny_reason)
+            await handle_application_review_result_interaction(self.__embed, interaction,
+                                                               "🙅 短講申請審查（已拒絕審查）",
+                                                               f"✅ 短講申請 ({self.__speech_id}) 已被拒絕。",
+                                                               deny_reason=deny_reason)
             print(f"Speech (id={self.__speech_id}) denied.")
         except NotFoundException as e:
             print(e)
@@ -44,28 +57,22 @@ class DenyReasonModal(discord.ui.Modal):
 
 class SpeechApplicationReviewView(discord.ui.View):
 
-    def __init__(self, embed, speech_application_repository: SpeechApplicationRepository):
+    def __init__(self, embed: discord.Embed, speech_application_repository: SpeechApplicationRepository):
         super().__init__()
         self.__embed = embed
         self.__speech_application_repository = speech_application_repository
 
     @discord.ui.button(label="通過申請", style=discord.ButtonStyle.success, emoji="🙆")
     async def accept_application(self, button: discord.ui.Button, interaction: discord.Interaction):
-        print(f"Speech (id={button.speech_id}) accepted.")
         speech_id = button.speech_id
 
         try:
             self.__speech_application_repository.update_speech_application_review_status(speech_id,
                                                                                          ApplicationReviewStatus.ACCEPTED)
-            self.__embed.title = "🙆 短講申請審查（已通過審查）"
-            self.__embed.description = (f"""{self.__embed.description}---
-審查者：<@{interaction.user.id}>
-審查時間：{convert_to_minguo_format(datetime.now())}
-""")
-            self.__embed.color = discord.Color.green()
-
-            await interaction.message.edit(embed=self.__embed, view=None)
-            await interaction.respond(f"✅ 短講申請 ({speech_id}) 已通過審查。", ephemeral=True)
+            await handle_application_review_result_interaction(self.__embed, interaction,
+                                                               "🙆 短講申請審查（已通過審查）",
+                                                               f"✅ 短講申請 ({speech_id}) 已通過審查。")
+            print(f"Speech (id={button.speech_id}) accepted.")
         except NotFoundException as e:
             print(e)
             await interaction.respond(f"Error: {str(e)}", ephemeral=True)
